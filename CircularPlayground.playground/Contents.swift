fae57211cda38a98c37f39658e740188cb725bc2
fgenolini <fgenolini@hotmail.com>
2015-08-23 23:59:21 +0100

    Cleanup
    Moved thread creation sample to ThreadPlayground, added CreateThread
    function

--- CircularPlayground.playground/Contents.swift
+++ CircularPlayground.playground/Contents.swift
@@ -1,69 +1,45 @@
-//: Playground - noun: a place where people can play
+
+//: Playground - circular buffer for multiple threads
 
 import Cocoa
 
+//: Number of times a thread has started
 var threadCalls = 0
 
-@objc class SayFunction {
-    func sayOne() {
-        NSThread.sleepForTimeInterval(5.0)
-        NSLog("First method invoked after 5s")
-        threadCalls++
-    }
-    
-    func sayTwo() {
-        NSThread.sleepForTimeInterval(10.0)
-        NSLog("Second method invoked after 10s")
-        threadCalls++
-    }
-    
-    func sayThree() {
-        NSThread.sleepForTimeInterval(15.0)
-        NSLog("Third method invoked after 15s")
-        threadCalls++
-    }
-}
-
-let hello = SayFunction()
-let thread1 = NSThread(target: hello, selector: "sayOne", object: nil)
-let thread2 = NSThread(target: hello, selector: "sayTwo", object: nil)
-let thread3 = NSThread(target: hello, selector: "sayThree", object: nil)
-thread1.start()
-thread2.start()
-thread3.start()
-
 func one() {
-    NSThread.sleepForTimeInterval(22.0)
-    NSLog("Method 1 invoked after 22s")
+    NSThread.sleepForTimeInterval(2.0)
+    NSLog("Method 1 invoked after 2s")
     threadCalls++
 }
-
 func two() {
-    NSThread.sleepForTimeInterval(25.0)
-    NSLog("Method 2 invoked after 25s")
+    NSThread.sleepForTimeInterval(5.0)
+    NSLog("Method 2 invoked after 5s")
     threadCalls++
 }
-
 func three() {
-    NSThread.sleepForTimeInterval(27.0)
-    NSLog("Method 3 invoked after 27s")
+    NSThread.sleepForTimeInterval(7.0)
+    NSLog("Method 3 invoked after 7s")
     threadCalls++
+}
+func CreateThread(block: dispatch_block_t!) {
+    dispatch_async(
+        dispatch_get_global_queue(
+            DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
+    block)
 }
 
 NSLog("Starting")
-dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
-    { three() })
-dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
-    { two() })
-dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
-    { one() })
+CreateThread({ three() })
+CreateThread({ two() })
+CreateThread({ one() })
 
-/*:
+/*: Infinite loop for testing only
 To be able to see the output of the threads,
-run the main thread for a long enough time:
+run the main thread for a long enough time, or forever:
+
     while(true) {}
 */
-while (threadCalls < 6) {
+while (threadCalls < 3) {
     NSThread.sleepForTimeInterval(1)
     threadCalls
 }
--- ThreadPlayground.playground/Contents.swift
+++ ThreadPlayground.playground/Contents.swift
@@ -1,5 +1,72 @@
-//: Playground - noun: a place where people can play
+
+//: Playground - different ways of creating threads in Swift
 
 import Cocoa
 
-var str = "Hello, playground"
+//: Number of times a thread has started
+var threadCalls = 0
+
+@objc class SayFunction {
+    func sayOne() {
+        NSThread.sleepForTimeInterval(5.0)
+        NSLog("First method invoked after 5s")
+        threadCalls++
+    }
+    func sayTwo() {
+        NSThread.sleepForTimeInterval(10.0)
+        NSLog("Second method invoked after 10s")
+        threadCalls++
+    }
+    func sayThree() {
+        NSThread.sleepForTimeInterval(15.0)
+        NSLog("Third method invoked after 15s")
+        threadCalls++
+    }
+}
+
+let hello = SayFunction()
+let thread1 = NSThread(target: hello, selector: "sayOne", object: nil)
+let thread2 = NSThread(target: hello, selector: "sayTwo", object: nil)
+let thread3 = NSThread(target: hello, selector: "sayThree", object: nil)
+thread1.start()
+thread2.start()
+thread3.start()
+
+func one() {
+    NSThread.sleepForTimeInterval(22.0)
+    NSLog("Method 1 invoked after 22s")
+    threadCalls++
+}
+func two() {
+    NSThread.sleepForTimeInterval(25.0)
+    NSLog("Method 2 invoked after 25s")
+    threadCalls++
+}
+func three() {
+    NSThread.sleepForTimeInterval(27.0)
+    NSLog("Method 3 invoked after 27s")
+    threadCalls++
+}
+func CreateThread(block: dispatch_block_t!) {
+    dispatch_async(
+        dispatch_get_global_queue(
+            DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
+        block)
+}
+
+NSLog("Starting")
+CreateThread({ three() })
+CreateThread({ two() })
+CreateThread({ one() })
+
+/*: Loop to keep main thread alive
+To be able to see the output of the threads,
+run the main thread for a long enough time, or forever:
+
+    while(true) {}
+*/
+while (threadCalls < 6) {
+    NSThread.sleepForTimeInterval(1)
+    threadCalls
+}
+
